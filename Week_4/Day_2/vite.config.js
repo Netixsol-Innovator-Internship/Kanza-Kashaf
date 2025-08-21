5d6e07a27c8ead0064c95caa001702ccb83004da
Kanza Kashaf <kanza_kashaf_intern@netixsol.com>
2025-08-21 12:19:19 +0500

    make changes to deploy week 4 day 2 task

--- Week_4/Day_2/vite.config.js
+++ Week_4/Day_2/vite.config.js
@@ -1,17 +1,17 @@
+// vite.config.js or vite.config.ts
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: "dist"
+    outDir: "dist",
   },
-  base: "./" // ✅ Important for Vercel deployment
+  base: "./", // ✅ Important for Vercel deployment
 })
