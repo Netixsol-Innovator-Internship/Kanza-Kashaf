6341cec03fc52981f1c80e7db2a6e4931115b4f9
Kanza Kashaf <kanza_kashaf_intern@netixsol.com>
2025-08-23 07:37:11 +0500

    Update swagger.js

--- Week_4/Day_5/backend/src/docs/swagger.js
+++ Week_4/Day_5/backend/src/docs/swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://kanzaweek4day5backendtask.vercel.app",
         description: "Development server",
       },
     ],
